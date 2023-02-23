7ab9e7d58039c360418c82da8f304b8e5e4d1d4a
Mehdi Hassan Jony <shoyshab@gmail.com>
2023-02-23 17:56:24 +0600

    error handling

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,9 +1,24 @@
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Category } from '../enum';
 
 export class UpdateBookDto {
+  @IsOptional()
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsString()
   readonly description: string;
+
+  @IsOptional()
+  @IsString()
   readonly author: string;
+
+  @IsOptional()
+  @IsNumber()
   readonly price: number;
+
+  @IsOptional()
+  @IsEnum(Category, { message: 'Please enter correct category.' })
   readonly category: Category;
 }
